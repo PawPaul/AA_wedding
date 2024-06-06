c1d770a3bf58c22b021cc0383cac597a63af3357
PawPaul <81380916+PawPaul@users.noreply.github.com>
2024-06-06 17:15:53 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 window.onload = function() {
     // alert('ciao2.onload ');
 }
+
+function sendMail() {
+
+    const userMessage = document.getElementById("userText").value;
+    console.log("Sending email with message:", userMessage);
+
+    // Logica mail
+
+
+
+    // Nascondiamo form e mostriamo messaggio di successo
+    const mailContainer = document.getElementById("messageForm");
+    mailContainer.hidden = true;
+
+    const messageThanks = document.getElementById("messageThanks");
+    messageThanks.hidden = false;
+}
